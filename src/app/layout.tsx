da497c5ce62c3bceefafb96cc4867aa04b535957
Krish-Parothi <krrishparothi10@gmail.com>
2026-05-22 08:05:10 +0530

    Removed Chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <AuthProvider>
           {children}
           <Toaster />
-          < FloatingChatAssistant />
+          
         </AuthProvider>
       </body>
     </html>
